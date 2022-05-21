5f46b4dd709ab4ae72337baa76e32c4e309c197b
Hao Zhang <hzhangxyz@outlook.com>
2022-05-21 11:18:10 +0800

    [visit_*_result] add visit_multiple_result and visit_single_result

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include <bits/utility.h>
 #include <iostream>
 #include <typeinfo>
 
@@ -20,4 +21,21 @@ int main() {
    std::cout << name(combine_variant<std::variant<int, double>, std::variant<int, float>>) << "\n";
    std::cout << name(refine_variant<std::variant<int, double, long>>) << "\n";
    std::cout << name(refine_variant<std::variant<std::variant<int, double>, std::variant<int, float>, float>>) << "\n";
+
+   std::cout << name(sequence_to_tuple_of_constant<std::index_sequence<1, 2, 3, 4>>) << "\n";
+   auto add = [](auto x, auto y) {
+      return x + y;
+   };
+   std::cout << name(visit_single_result<decltype(add), std::index_sequence<0, 0>, std::variant<int, double>, std::variant<long, float>>) << "\n";
+   std::cout << name(visit_single_result<decltype(add), std::index_sequence<0, 1>, std::variant<int, double>, std::variant<long, float>>) << "\n";
+   std::cout << name(visit_single_result<decltype(add), std::index_sequence<1, 0>, std::variant<int, double>, std::variant<long, float>>) << "\n";
+   std::cout << name(visit_single_result<decltype(add), std::index_sequence<1, 1>, std::variant<int, double>, std::variant<long, float>>) << "\n";
+   std::cout << name(tupled_visit_multiple_result<
+                      decltype(add),
+                      std::tuple<std::variant<int, double>, std::variant<long, float>>,
+                      std::index_sequence<0, 0>,
+                      std::index_sequence<0, 1>,
+                      std::index_sequence<1, 0>,
+                      std::index_sequence<1, 1>>)
+             << "\n";
 }
--- variant_toolkit.hpp
+++ variant_toolkit.hpp
@@ -19,7 +19,10 @@
 #ifndef VARIANT_TOOLKIT_HPP
 #define VARIANT_TOOLKIT_HPP
 
+#include <array>
+#include <tuple>
 #include <type_traits>
+#include <utility>
 #include <variant>
 
 namespace variant_toolkit {
@@ -79,6 +82,48 @@ namespace variant_toolkit {
    struct refine_variant_helper<Result, std::variant<>> : type_identity<Result> {};
    template<typename Variant>
    using refine_variant = typename refine_variant_helper<std::variant<std::monostate>, Variant>::type;
+
+   template<std::size_t Index, typename T>
+   struct tuple_parameter_helper;
+   template<std::size_t Index, typename Head, typename... Tail>
+   struct tuple_parameter_helper<Index, std::tuple<Head, Tail...>> : tuple_parameter_helper<Index - 1, std::tuple<Tail...>> {};
+   template<typename Head, typename... Tail>
+   struct tuple_parameter_helper<0, std::tuple<Head, Tail...>> : type_identity<Head> {};
+   template<std::size_t Index, typename T>
+   struct template_parameter_helper;
+   template<std::size_t Index, template<typename...> class Template, typename... Args>
+   struct template_parameter_helper<Index, Template<Args...>> : tuple_parameter_helper<Index, std::tuple<Args...>> {};
+   template<std::size_t Index, typename T>
+   using template_parameter = typename template_parameter_helper<Index, T>::type;
+
+   template<typename Sequence>
+   struct sequence_to_tuple_of_constant_helper : type_identity<std::tuple<>> {};
+   template<typename Type, Type... Value>
+   struct sequence_to_tuple_of_constant_helper<std::integer_sequence<Type, Value...>> :
+         type_identity<std::tuple<std::integral_constant<Type, Value>...>> {};
+   template<typename Sequence>
+   using sequence_to_tuple_of_constant = typename sequence_to_tuple_of_constant_helper<Sequence>::type;
+
+   template<typename Visitor, typename Indices, typename Variants, std::size_t... IndexSequence>
+   using visit_single_result_helper = std::invoke_result_t<
+         Visitor,
+         std::variant_alternative_t<template_parameter<IndexSequence, Indices>::value, template_parameter<IndexSequence, Variants>>...>;
+   template<typename Visitor, typename Indices, typename Variants, typename Sequence>
+   struct visit_single_result_passer : type_identity<void> {};
+   template<typename Visitor, typename Indices, typename Variants, std::size_t... IndexSequence>
+   struct visit_single_result_passer<Visitor, Indices, Variants, std::integer_sequence<std::size_t, IndexSequence...>> :
+         type_identity<visit_single_result_helper<Visitor, Indices, Variants, IndexSequence...>> {};
+   template<typename Visitor, typename Indices, typename Variants>
+   using tupled_visit_single_result = typename visit_single_result_passer<
+         Visitor,
+         sequence_to_tuple_of_constant<Indices>,
+         Variants,
+         std::make_index_sequence<std::tuple_size_v<Variants>>>::type;
+   template<typename Visitor, typename Indices, typename... Variants>
+   using visit_single_result = tupled_visit_single_result<Visitor, Indices, std::tuple<Variants...>>;
+
+   template<typename Visitor, typename Variants, typename... Indices>
+   using tupled_visit_multiple_result = refine_variant<std::variant<tupled_visit_single_result<Visitor, Indices, Variants>...>>;
 
 } // namespace variant_toolkit
 
